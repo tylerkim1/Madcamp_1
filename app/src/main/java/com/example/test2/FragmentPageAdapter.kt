83f42925502739fe2c746996d3bfdf384e4f28aa
tylerkim <tylerkim129@gmail.com>
2023-06-30 12:04:33 +0900

    tab

---
+++ app/src/main/java/com/example/test2/FragmentPageAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.test2
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FragmentPageAdapter (
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle){
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> FirstFragment()
+            1 -> SecondFragment()
+            else -> ThirdFragment()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.test2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var adapter: FragmentPageAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager2 = findViewById(R.id.viewPager2)
+
+        adapter = FragmentPageAdapter(supportFragmentManager , lifecycle)
+
+        tabLayout.addTab(tabLayout.newTab().setText("First"))
+        tabLayout.addTab(tabLayout.newTab().setText("Second"))
+        tabLayout.addTab(tabLayout.newTab().setText("Third"))
+
+        viewPager2.adapter = adapter
+
+        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+    }
+}
