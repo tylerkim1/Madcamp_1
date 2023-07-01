9db55fee85920ec6882ddbc216a7ca483c2cad4d
tylerkim <tylerkim129@gmail.com>
2023-07-01 15:04:03 +0900

    initial card view

---
+++ app/src/main/java/com/example/test2/User.java
@@ -0,0 +1,13 @@
+package com.example.test2;
+
+public class User {
+    String name, phoneNo, info;
+    int imageId;
+
+    public User(String name, String phoneNo, String info, int imageId) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.info = info;
+        this.imageId = imageId;
+    }
+}
--- app/src/main/java/com/example/test2/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.test2
-
-class User(
-    var name: String,
-    var phoneNo: String,
-    var info: String,
-    var imageId: Int
-    )
