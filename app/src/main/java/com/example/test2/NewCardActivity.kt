7b864630413f8d7e3c00015c1e5d00991562d69b
tylerkim <tylerkim129@gmail.com>
2023-07-03 21:46:50 +0900

    Image shown in the new card activity

--- app/src/main/java/com/example/test2/NewCardActivity.kt
+++ app/src/main/java/com/example/test2/NewCardActivity.kt
@@ -6,13 +6,16 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.test2.databinding.ActivityNewCardBinding
 
 class NewCardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewCardBinding
-    private lateinit var selectedImageUri: Uri
+    private var selectedImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +28,19 @@ class NewCardActivity : AppCompatActivity() {
             startActivityForResult(intent, REQUEST_SELECT_IMAGE)
         }
 
-
         // 추가 버튼 클릭 시 새로운 카드 생성
         binding.addCardButton.setOnClickListener {
             val text = binding.editText.text.toString()
+
+            if (selectedImageUri == null) {
+                Toast.makeText(this, "사진을 선택해주세요!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(text)) {
+                Toast.makeText(this, "추억을 입력해주세요!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             val resultIntent = Intent().apply {
                 putExtra("imageUri", selectedImageUri.toString()) // Convert Uri to String
                 putExtra("text", text)
@@ -42,7 +54,10 @@ class NewCardActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_SELECT_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
             selectedImageUri = data.data!!
-            // 선택된 이미지를 이미지 뷰에 표시하는 코드 추가
+            // 선택된 이미지를 이미지 뷰에 표시
+            Glide.with(this)
+                .load(selectedImageUri)
+                .into(binding.imageView)
         }
     }
 
